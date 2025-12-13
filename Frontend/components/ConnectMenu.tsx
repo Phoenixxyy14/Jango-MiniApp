dd1be1b0b8f0c31b66eb8b8861d40215438fc9e1
Saber1Y <ayodejiakintobi1@gmail.com>
2025-12-13 11:58:05 +0100

    feat: wagmi & viem setup

---
+++ Frontend/components/ConnectMenu.tsx
@@ -0,0 +1,28 @@
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+
+export default function ConnectMenu() {
+  const { isConnected, address } = useAccount();
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect();
+
+  if (isConnected) {
+    return (
+      <>
+        <div>You're connected!</div>
+        <div>Address: {address}</div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => connect({ connector: connectors[0] })}
+      >
+        Connect
+      </button>
+      <button onClick={() => disconnect()}>Disconnect</button>
+    </>
+  );
+}
